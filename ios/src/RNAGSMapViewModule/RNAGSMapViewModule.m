b806b6a652e9f65eb3e44a10edfac60b19e1f9d6
David Galindo <jisadg0@MacBook149.local>
2019-02-18 13:27:09 -0600

    Added Licensing as a React method cal

---
+++ ios/src/RNAGSMapViewModule/RNAGSMapViewModule.m
@@ -0,0 +1,9 @@
+//
+//  RNAGSMapViewModule.m
+//  RNArcGISMapView
+//
+//  Created by David Galindo on 2/18/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/src/RNAGSMapViewModule/RNArcGISMapViewModule.swift
@@ -0,0 +1,22 @@
+//
+//  RNAGSMapViewModule.swift
+//  RNArcGISMapView
+//
+//  Created by David Galindo on 2/18/19.
+//  Copyright © 2019 David Galindo. All rights reserved.
+//
+
+import ArcGIS
+import Foundation
+
+@objc(RNAGSMapViewModule)
+public class RNAGSMapViewModule: NSObject{
+    @objc func setLicenseKey(_ key: String) {
+        do {
+            try AGSArcGISRuntimeEnvironment.setLicenseKey("key")
+        }
+        catch let error as NSError {
+            print("error: \(error)")
+        }
+    }
+}
